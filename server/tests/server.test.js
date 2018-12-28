63c9138052994f70dd209bd5c7e7e4cc355b012f
Ly Cao <lycao@10-23-0-196.dynapool.nyu.edu>
2018-12-28 18:30:52 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -346,6 +346,26 @@ describe('POST /users/login', ()=>{
     });
 });
 
+describe('DELETE /users/me/token', ()=>{
+    //real x-auth passed out to log out method=> get logged out
+    it('should remove auth token on logout', (done)=>{
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+
+            User.findById(users[0]._id).then((user)=>{
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e)=> done(e));
+        });
+    });
+})
+
 
 
 
